57831821957d14a85c380b7a552c8fc7e3c303c9
dimacv <47975357+dimacv@users.noreply.github.com>
2023-06-14 10:06:36 +0300

    Update BingChat2.py

--- BingChat2.py
+++ BingChat2.py
@@ -3,19 +3,19 @@ from EdgeGPT.EdgeUtils import Query, Cookie
 #import streamlit_session_state as session_state
 
 cookie_file = "./bing_cookies_1.json"
-proxy = 'http://neon:qwerty123@dc1.rbr.ru:4000'
+proxy = ''
 
 # Функция для отправки запроса к Bing Chat
 def send_request_to_bingchat(user_input):
     q = Query(user_input,
               style="precise",  # or: 'creative', 'balanced', 'precise'
               cookie_file=cookie_file,
-              proxy=proxy)
+              #proxy=proxy
+             )
 
     return [q.output, q.suggestions]
 
-def authenticate(username, password):
-    return username == 'dimacv' and password == 'vadgra'
+
 
 # Главная функция для создания веб-приложения с использованием Streamlit
 def app():
@@ -38,24 +38,5 @@ def app():
 # Запуск приложения
 if __name__ == "__main__":
 
-    # Получение сеансового состояния
-    session_state = st.session_state
-
-    if "authenticated" not in session_state:
-        session_state.authenticated = False
-
-    if not session_state.authenticated:
-        username_placeholder = st.empty()
-        password_placeholder = st.empty()
-        username = username_placeholder.text_input("Username:")
-        password = password_placeholder.text_input("Password:", type="password")
-        if st.button("Login"):
-            if authenticate(username, password):
-                session_state.authenticated = True
-                username_placeholder.empty()
-                password_placeholder.empty()
-                app()
-            else:
-                st.error("Wrong credentials, please try again.")
-    else:
-        app()
+    # 
+    app()
